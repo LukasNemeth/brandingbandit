d63e030e1e7978fd7fb394a172059e4d83bd788a
LukasNemeth <167068044+LukasNemeth@users.noreply.github.com>
2024-04-16 12:20:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-// Add this script to the bottom of your body tag
-const ctaBar = document.getElementById('cta-bar');
-window.onscroll = function() {
-  if (window.scrollY > 100) {
-    ctaBar.style.display = 'block';
-  } else {
-    ctaBar.style.display = 'none';
-  }
-};
 
